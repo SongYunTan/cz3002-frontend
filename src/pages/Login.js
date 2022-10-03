d835f92f19c2c498812c8f18120b7ab5d1add4b1
SongYunTan <songyun.tan@gmail.com>
2022-10-03 23:38:36 +0800

    Login page skeleton

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,37 @@
 import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Login = () => {
+  const logo = "logo.png"
   return (
-    <Link to="/login">
-      <button variant="outlined">
-        Log In
-      </button>
-    </Link>
+    <Container id="login">
+      <Col>
+        <img className="image" src={logo}/>
+        <form>
+          <Row>
+            <label>
+              Email:
+              <input type="text" name="email" />
+            </label>
+          </Row>
+          <Row>
+            <label>
+              Password:
+              <input type="text" name="password" />
+            </label>
+          </Row>
+          <br></br>
+          <input type="submit" value="LOG IN" className="button"/>
+        </form>
+    
+        <p className="signup-link">
+          New to Tindflix? 
+          <Link to="/signup">Sign Up</Link>
+        </p>
+      </Col>
+    </Container>
   );
 }
   
