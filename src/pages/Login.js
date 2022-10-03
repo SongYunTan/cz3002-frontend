8e03c466d445e6338a5ca33d4cd8d9517c9b5d48
SongYunTan <songyun.tan@gmail.com>
2022-10-04 00:53:37 +0800

    Put input labels within text input

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,25 @@
+import React, {useState} from 'react';
 import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
+function TextInput({ type, label }) {
+  const [value, setValue] = useState('');
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return (
+    <div className="input-container">
+      <input type={type} value={value} onChange={handleChange} />
+      <label className={value && 'filled'}>
+        {label}
+      </label>
+    </div>
+  );
+}
 
 const Login = () => {
   const logo = "logo.png"
@@ -11,16 +29,10 @@ const Login = () => {
         <img className="image" src={logo}/>
         <form>
           <Row>
-            <label>
-              Email:
-              <input type="text" name="email" />
-            </label>
+            <TextInput type="text" label="Email"/>
           </Row>
           <Row>
-            <label>
-              Password:
-              <input type="text" name="password" />
-            </label>
+            <TextInput type="password" label="Password"/>
           </Row>
           <br></br>
           <input type="submit" value="LOG IN" className="button"/>
