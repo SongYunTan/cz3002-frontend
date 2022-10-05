3d08ec5d96d35bba3e850a833b5aaf0edea43bd3
SongYunTan <songyun.tan@gmail.com>
2022-10-05 12:07:51 +0800

    Added matches call to endpoint

--- src/pages/Matches/Matches.js
+++ src/pages/Matches/Matches.js
@@ -5,16 +5,41 @@ import './Matches.css';
 import {Matcheslist} from './Matcheslist';
 
 const Matches = () => {
+  const [matchedMovies, setMatchedMovies] = useState([]);
+
+  const getData = async () => {
+
+    await axios.post(
+      'http://127.0.0.1:5000/match_movie',
+      {groupName: "test"},
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      },
+    ).catch((err) => {
+      console.log(err.message);
+    }).then((response)=> {
+      console.log(JSON.stringify(response.data, null, 4));
+      setMatchedMovies(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className='matches'>
       <Navbar />
       <div className='groupMatches'>
         <div className='groupNav-bar'><GroupNavbar /></div>
         <div className="groupMatches-content">
-          {Matcheslist.map((val, key) =>{
+          {matchedMovies.map((val, key) =>{
               return (
                 <div className="movie">
-                  <img className='matchesMoviePoster' alt='movie poster' src={'images/' + val.image}></img>
+                  <img className='matchesMoviePoster' alt='movie poster' src={val.url}></img>
                   <p>{val.title}</p>
                 </div>
               )
